28ba978766e859eaf4120f6b492bfcc4b599e944
Andrii Sadovyi <waterpolomen2@gmail.com>
2023-02-01 11:34:38 +0100

    added model classes

---
+++ lib/models/workout.dart
@@ -0,0 +1,28 @@
+import 'package:equatable/equatable.dart';
+
+import 'exercise.dart';
+
+class Workout extends Equatable {
+  final String? title;
+  final List<Exercise> exercises;
+
+  const Workout({
+    required this.title,
+    required this.exercises,
+  });
+  factory Workout.fromJson(Map<String, dynamic> json) {
+    List<Exercise> exercises = [];
+    int index = 0;
+    int startTime = 0;
+    for (var ex in (json['exercises'] as Iterable)) {
+      exercises.add(Exercise.fromJson(json, index, startTime));
+      index++;
+      startTime += exercises.last.prelude! + exercises.last.duration!;
+    }
+    return Workout(title: json['title'] as String?, exercises: exercises);
+  }
+  Map<String, dynamic> toJson() => {'title': title, 'exercises': exercises};
+
+  @override
+  List<Object?> get props => [title, exercises];
+}
